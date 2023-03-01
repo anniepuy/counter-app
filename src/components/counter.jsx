26483909babe8d7ace9beeb532072b36d5a2419a
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-03-01 13:36:18 -0600

    moving class to method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,12 +6,9 @@ class Counter extends Component {
     };
     
     render() { 
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-
         return (
             <>
-                <span className= {classes}>{this.formatCount()}</span>
+                <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className = "btn btn-secondary btn-sm">Increment</button>
             </>
         );
@@ -21,6 +18,12 @@ class Counter extends Component {
         const { count } = this.state;
 
         return count === 0 ? 'Zero' : count;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 }
  
