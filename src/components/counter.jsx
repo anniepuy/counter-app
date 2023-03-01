dbaf32ea31588b83b2819ced2c6bdf10cd0b6f22
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-03-01 14:02:42 -0600

    adding a key to the arrray

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state ={ 
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: [
+            'tag1', 
+            'tag2', 
+            'tag3',
+        ]
     };
     
     render() { 
@@ -12,7 +16,7 @@ class Counter extends Component {
                 <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className = "btn btn-secondary btn-sm">Increment</button>
                 <ul>
-                    {this.state.tags.map(tag => <li>{tag}</li>)}
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
                 </ul>
             </>
         );
