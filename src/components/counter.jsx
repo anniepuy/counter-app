3aad05bc6b19fd65fac8522c73757e98cb923a0d
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-03-03 09:45:22 -0600

    remove list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,20 +5,13 @@ class Counter extends Component {
         count: 0,
         tags: []
     };
-
-    renderTags() {
-        if (this.state.tags.length === 0) return <p>Looks like there's nothing here!</p>;
-
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-    }
-    
+ 
     render() { 
         return (
             <>
                 <span className= {this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className = "btn btn-secondary btn-sm">Increment</button>
-                { this.state.tags.length === 0 && "Please create a new tag"}
-               {this.renderTags()}
+    
             </>
         );
     }
